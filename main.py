49ed707bfc618efaff8af8a5ec6bc40059b2bbb8
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-03-15 05:29:41 +0000

    created test integers, converted to strings, and converted to char arrays

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #Given an integer x, return true if x is a palindrome, and false otherwise.
 
+#test ints
+intA = 6556
+intB = 6731
+
+#convert my ints to strings
+strA = str(intA)
+strB = str(intB)
+
+#convert strings to char array
+listA = list(strA)
+listB = list(strB)
 
